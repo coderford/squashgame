668fbb0e8243dd3313bee3619ea82ebb25eb332e
Aditya Kumar <kumar.sava.aditya@gmail.com>
2017-09-13 03:06:08 +0530

    add highscore system

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Requires pygame to be installed on your python3 environment in order to play.
 
 ## Controls
 * Move the paddle with left and right keys
-* Press Return to pause
+* Press Space to pause
 * Press Escape to quit
--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
-import pygame, time, sys
+import pygame, sys, csv, os, eztext
+from math import fabs
+from time import sleep
 
 # global variables:
-screen_x = 640
-screen_y = 480
+screen_x = 640 #screen width
+screen_y = 480 #screen height
 paddle = {
     'width': 70,
     'height': 10,
@@ -25,41 +27,93 @@ paused = False
     # some colors:
 white = (255, 255, 255)
 black = (0, 0, 0)
-red = (255, 0, 0)    
-
-# initializations:
-pygame.init()
-pygame.display.set_caption('Squash')
-screen = pygame.display.set_mode((screen_x, screen_y))
-clock = pygame.time.Clock()
+red = (255, 0, 0)
+    #highscore vars
+highscores = []  
+highscore_file = 'highscores.csv'   
 
 # functions:
+def read_highscores(filename):
+    global highscores
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            highscores.append(tuple(row))
+
+def write_highscores(filename):
+    global highscores
+    with open(os.path.join(os.path.dirname(__file__), filename), 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(highscores)
+
 def gameOver():
     screen.fill(black)
     myFont = pygame.font.SysFont('sans serif', 72)
     GOsurf = myFont.render('GAME OVER', True, white) #2nd arg for AA
     GOrect = GOsurf.get_rect()
     GOrect.midtop = (320, 220)   
+    for i, highscore in enumerate(highscores): # checking if there is a new highscore and adding to highscores
+        if score>int(highscore[1]):
+            txtbx = eztext.Input(x = 20, y = 220,maxlength=45, color=(255,255,255), prompt='NEW HIGHSCORE! Your name: ')
+            input_done = False
+            name = ''
+            while not input_done:
+                events = pygame.event.get()
+                for event in events:
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                        name = txtbx.value
+                        input_done = True
+                        print(name)
+                txtbx.update(events)
+                screen.fill(black)
+                txtbx.draw(screen)
+                pygame.display.flip()
+            highscores.insert(i, tuple([name, str(score)])) 
+            highscores.pop()
+            print(highscores)
+            break
+    # now blitting and drawing everything after getting input
+    screen.fill(black)
     screen.blit(GOsurf, GOrect)
     showScore(1)
     pygame.display.flip()
-    time.sleep(2)
+    write_highscores(highscore_file)
+    sleep(2)   # wait for 2 secs 
     pygame.quit()   #pygame exit
     sys.exit()  #console exit
 
-def showScore(mode = 0):
+def showScore(mode = 0): # mode: 0 means normal corner display, 1 means gameover display with highscores
+    global score, screen
     myFont = pygame.font.SysFont('sans serif', 30)
+    myFont_small = pygame.font.SysFont('mono', 18)
     if mode == 0:
-        scoreSurf = myFont.render(str(score), True, white)
-        scoreRect = scoreSurf.get_rect()
-        scoreRect.midtop = (20, 20)
+        myscoreSurf = myFont.render(str(score), True, white)
+        myscoreRect = myscoreSurf.get_rect()
+        myscoreRect.midtop = (20, 20)
     else:
-        scoreSurf = myFont.render('Your score: '+str(score), True, white)
-        scoreRect = scoreSurf.get_rect()
-        scoreRect.midtop = (320, 300)
-    screen.blit(scoreSurf, scoreRect)
+        myscoreSurf = myFont.render('Your score: '+str(score), True, white)
+        myscoreRect = myscoreSurf.get_rect()
+        myscoreRect.midtop = (320, 270)
+        high_title = myFont.render('High Scores', True, white)
+        high_titleRect = high_title.get_rect()
+        high_titleRect.midtop = (320, 315)
+        screen.blit(high_title, high_titleRect)
+        highscore_screens = []
+        highscore_rects = []
+        score_start_pos = [320, 350]
+        for i, highscore in enumerate(highscores):
+            highscore_screens.append(myFont_small.render(str(i+1)+'. '+"{:14}".format(highscore[0]) + '{:3}'.format(highscore[1]), True, white))
+        for high_screen in highscore_screens:
+            highscore_rects.append(high_screen.get_rect())
+        for rect in highscore_rects:
+            rect.midtop = tuple(score_start_pos)
+            score_start_pos[1]+=18
+        for high_screen, rect in zip(highscore_screens, highscore_rects):
+            screen.blit(high_screen, rect)
+    screen.blit(myscoreSurf, myscoreRect)
 
 def pauseScreen():
+    global screen
     translucentBkg = pygame.Surface((screen_x, screen_y), pygame.SRCALPHA)
     translucentBkg.fill((50, 50, 50, 100))
     myFont = pygame.font.SysFont('sans serif', 50)
@@ -77,7 +131,7 @@ def handle_events():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 done = True
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_SPACE:
                 paused = not paused
 
 def paddle_updates():
@@ -99,6 +153,7 @@ def ball_updates():
         # with walls:
     if ball['x']+ball['size'] > screen_x or ball['x'] < 0:
         ball['x_speed'] = -ball['x_speed']
+        ball['x'] += 2*ball['x_speed']/fabs(ball['x_speed'])
     if ball['y'] < 0:
         ball['y_speed'] = -ball['y_speed']
         # with paddle:
@@ -121,6 +176,13 @@ def draw():
         pauseScreen()
     pygame.display.flip()
     clock.tick(framerate)
+
+# initializations:
+pygame.init()
+pygame.display.set_caption('Squash')
+screen = pygame.display.set_mode((screen_x, screen_y))
+clock = pygame.time.Clock()
+read_highscores(highscore_file)
 
 # main loop:
 while not done:
